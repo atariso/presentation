b6be17d14076c7db9e3216a2eaac87ecc52cfce5
atariso <s.naka.30912@gmail.com>
2021-01-12 03:53:13 +0900

    reformatted code changed comment

--- mip.py
+++ mip.py
@@ -7,11 +7,11 @@ cost_limit = 2000  # 一日何円までか
 protein_lower_limit = 120  # タンパク質の摂取目標
 fat_lower_limit = 50  # 脂質の摂取目標
 calorie_limit = 1500  # カロリー制限
+repeat_limit = 3  # 同じものを一日何度まで食べるか
 
 df = pd.read_csv('meal.csv')
-
 names = df.name
-cost = df.yen  # 自炊する想定。現実的に作れないものは購入
+cost = df.yen
 protein = df.P
 fat = df.F
 carbohydrate = df.C
@@ -27,7 +27,6 @@ for i in range(meal_num):
     # 一日三食
     for j in range(3):
         x[i, j] = solver.IntVar(0, 1, '')
-        # x[i, j] = solver.IntVar(0, 1, '')
 
 # 一日の食事代の合計に関する制約
 solver.Add(solver.Sum([x[i, j] * cost[i] for i in range(meal_num) for j in range(3)]) <= cost_limit)
@@ -43,11 +42,11 @@ solver.Add(solver.Sum([x[i, j] * calorie[i] for i in range(meal_num) for j in ra
 
 # 任意の食材は一日に1度までしか食べられない（飽きるので）
 for i in range(meal_num):
-    solver.Add(solver.Sum([x[i, j] for j in range(3)]) <= 1)
+    solver.Add(solver.Sum([x[i, j] for j in range(3)]) <= repeat_limit)
 
 # 任意の食事は300kcal以上を摂取する（腹が減るので）
 for j in range(3):
-    solver.Add(solver.Sum([x[i, j]*calorie[i] for i in range(meal_num)]) >= 300)
+    solver.Add(solver.Sum([x[i, j] * calorie[i] for i in range(meal_num)]) >= 300)
 
 solver.Maximize(solver.Sum([satisfaction[i] * x[i, j] for i in range(meal_num) for j in range(3)]))
 
